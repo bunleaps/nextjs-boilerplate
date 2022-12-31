a69916483bbfe72455afb376caf47fdf57804d58
bunleaps <bunleap.sorn@gmail.com>
2022-12-31 14:16:15 +0700

    update font: Work Sans variant

--- pages/_document.js
+++ pages/_document.js
@@ -17,7 +17,7 @@ class MyDocument extends Document {
             crossOrigin="true"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
         </Head>
